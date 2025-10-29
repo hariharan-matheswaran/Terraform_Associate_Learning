d3d5a457b4832799aebf7120361853a6d5352a28
hariharan-matheswaran <harimalar99m@gmail.com>
2025-10-29 07:32:53 +0530

    created providers.tf and creating resources of Ec2 and github

--- github.tf
+++ github.tf
@@ -1,14 +1,5 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.0"
-    }
-  }
-}
-
-/*resource "github_repository" "example" {
+resource "github_repository" "example" {
   name        = "example"
   description = "My awesome codebase"
   visibility = "public"
-}*/
+}
